066004f07fcd2e7bc13e75b0ecf448cca3c8c674
jtristan <trjohnb@amazon.com>
2023-12-14 09:23:59 -0500

    Improved verify script for use from submodule

--- verify.sh
+++ verify.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DIR=`dirname "$0"`
+
 echo Verifying the Dafny code from the lectures
 
-for dfyfile in `ls Lectures/Code/*.dfy`
+for dfyfile in `ls $DIR/Lectures/Code/*.dfy`
 do
     echo '...' Verifying $dfyfile
     dafny verify $dfyfile
@@ -10,7 +12,7 @@ done
 
 echo Verifying the solutions to the exercises
 
-for dfyfile in `ls Exercises/*_solution.dfy`
+for dfyfile in `ls $DIR/Exercises/*_solution.dfy`
 do
     echo '...' Verifying $dfyfile
     dafny verify $dfyfile
